37911c63ae7dafacdf7ce0aebea704e07d0d9ba6
LYF7314 <1650969935@qq.com>
2023-07-06 14:44:48 +0800

    下拉框选建筑见楼层，选楼层见房间

--- src/components/LayoutPage/MainWindow.vue
+++ src/components/LayoutPage/MainWindow.vue
@@ -1,9 +1,21 @@
 <template>
     <div id="mainWindow">
         <div id="floorItems">
-            <span class="floorInfo">办公楼:</span><div style="width:93px" class="selectItem"><el-select size="mini" id="ffs"></el-select></div>
-            <span class="floorInfo">楼层:</span>  <div class="selectItem"><el-select id="sfs" size="mini"></el-select></div>
-            <span class="floorInfo">房间号:</span><div class="selectItem"><el-select id="tfs" size="mini"></el-select></div>
+            <!-- 办公楼下拉框 -->
+            <span class="floorInfo">办公楼:</span>
+            <div style="width:93px" class="selectItem"><el-select clearable size="mini" id="ffs" v-model="buildingOption"
+                    @change="updateFloorOption"> <el-option v-for="option in BuildingOptions" :key="option.value"
+                        :label="option.label" :value="option.value"></el-option></el-select></div>
+            <!-- 楼层下拉框 -->
+            <span class="floorInfo">楼层:</span>
+            <div class="selectItem"><el-select clearable id="sfs" size="mini" v-model="floorOption"
+                    @change="updateRoomOption"><el-option v-for="option in FloorOptions" :key="option.value"
+                        :label="option.label" :value="option.value"></el-option></el-select></div>
+            <!-- 房间下拉框 -->
+            <span class="floorInfo">房间号:</span>
+            <div class="selectItem"><el-select clearable id="tfs" size="mini" v-model="roomOption">
+                    <el-option v-for="option in RoomOptions" :key="option.value" :label="option.label"
+                        :value="option.value"></el-option></el-select></div>
         </div>
         <div id="refreshDiv">
             <span class="innerSpan">保存</span>
@@ -11,7 +23,7 @@
         </div>
         <div id="addDiv">
             <span class="innerSpan"></span>
-            <el-button @click="addPage"  icon="el-icon-circle-plus-outline" class="innerText" type="primary"></el-button>
+            <el-button @click="addPage" icon="el-icon-circle-plus-outline" class="innerText" type="primary"></el-button>
         </div>
         <div id="changeDiv">
             <span class="innerSpan"></span>
@@ -30,94 +42,139 @@
 
 <script>
 export default {
-    methods:{
-        addPage(){
+    data() {
+        return {
+            buildingOption: '',
+            floorOption: '',
+            roomOption: '',
+            BuildingOptions: [],
+            FloorOptions: [],
+            RoomOptions: [],
+        }
+    },
+    created() {
+        // this.$http.get('/manage/infos').then(response => {
+        //     this.BuildingOptions = response.data.areas.map(area => area.buildingName);
+        // }).catch(error => {
+        //     console.log(error);
+        // });
+    },
+    methods: {
+        addPage() {
             this.$emit('addPage');
         },
-        changePage(){
+        changePage() {
             this.$emit('changePage');
         },
-        deletePage(){
+        deletePage() {
             this.$emit('deletePage');
+        },
+        updateFloorOption() {
+            // const area = this.$data.BuildingOptions.find(area => area.buildingName === this.buildingOption);
+            // this.FloorOptions = area.floors.map(floor => floor.floorName);
+            // this.floorOption = '';
+            // this.roomOption = '';
+        },
+        updateRoomOption() {
+            // const area = this.$data.BuildingOptions.find(area => area.buildingName === this.buildingOption);
+            // const floor = area.floors.find(floor => floor.floorName === this.floorOption);
+            // this.RoomOptions = floor.rooms.map(room => room.name);
+            // this.roomOption = '';
         }
     }
 }
+
 </script>
+
 <style lang="scss" scoped>
-*{
+* {
     margin: 0;
     padding: 0;
 }
-#mainWindow{
+
+#mainWindow {
     position: relative;
     height: 770px;
     width: 970px;
     background-color: white;
     border-radius: 4px;
 }
-#floorItems{
+
+#floorItems {
     position: absolute;
     top: 13px;
     left: 20px;
 }
-.floorInfo{
+
+.floorInfo {
     display: inline-block;
     height: 23px;
     font-size: 14px;
     font-family: '微软雅黑';
-    color: rgb(51,51,51);
+    color: rgb(51, 51, 51);
     margin-left: 10px;
 }
-.selectItem{
+
+.selectItem {
     display: inline-block;
     margin-left: 5px;
 }
-/deep/#sfs{
+
+/deep/#sfs {
     padding-left: 4px;
     width: 75px;
     height: 25px;
 }
-/deep/#tfs{
+
+/deep/#tfs {
     padding-left: 4px;
     width: 75px;
     height: 25px;
 }
-/deep/#ffs{
+
+/deep/#ffs {
     padding-left: 4px;
     width: 93px;
     height: 25px;
 }
-.innerText{
+
+.innerText {
     position: relative;
     height: 30px;
     width: 37px;
 }
-#saveText{
+
+#saveText {
     position: relative;
     height: 23px;
     width: 60px;
 }
-#refreshDiv{
-    position:absolute;
-    left:850px;
-    top:14px;
+
+#refreshDiv {
+    position: absolute;
+    left: 850px;
+    top: 14px;
 }
-#addDiv{
-    position:absolute;
-    left:500px;
-    top:12px;
+
+#addDiv {
+    position: absolute;
+    left: 500px;
+    top: 12px;
 }
-#changeDiv{
-    position:absolute;
-    left:450px;
-    top:12px;
+
+#changeDiv {
+    position: absolute;
+    left: 450px;
+    top: 12px;
 }
-#deleteDiv{
-    position:absolute;
-    left:550px;
-    top:12px;
+
+#deleteDiv {
+    position: absolute;
+    left: 550px;
+    top: 12px;
 }
-.innerSpan{
+
+.innerSpan {
     position: absolute;
     top: 2px;
     left: 15px;
@@ -128,7 +185,7 @@ export default {
     pointer-events: none;
 }
 
-#canvasBox{
+#canvasBox {
     position: absolute;
     top: 50px;
     left: 41px;
